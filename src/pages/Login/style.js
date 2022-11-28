8228384d5724004b2c6a11da303e7c39c297bc5e
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-11-27 21:08:24 -0300

    Criação do layout do componente LoginForm
    Criação do layout do componente LoginForm, para fazer login do sistema.

---
+++ src/pages/Login/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+
+    .cover-container{
+        position: absolute;
+        width: 674px;
+        height: 774px;
+        left: 0px;
+        top: 0px;
+        background: #2541B2;
+    }
+
+    .login-container{
+        position: absolute;
+        top: 238px;
+        right: 200px;
+    }
+`;
