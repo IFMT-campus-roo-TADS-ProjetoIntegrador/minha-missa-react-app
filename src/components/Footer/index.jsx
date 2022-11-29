026f3232120d74fae60544cae0d02835c849e281
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-11-29 11:53:19 -0300

    Criação do elemento Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Footer(){
+    return (
+        <div class="container">
+            <footer class="py-3 my-4 footer">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><Link to="/home" class="nav-link px-2 text-muted">Início</Link></li>
+                    <li class="nav-item"><Link to="/" class="nav-link px-2 text-muted">Eventos</Link></li>
+                    <li class="nav-item"><Link to="/" class="nav-link px-2 text-muted">Instituições</Link></li>
+                    <li class="nav-item"><Link to="/" class="nav-link px-2 text-muted">Sobre Nós</Link></li>
+                </ul>
+                <p class="text-center text-muted">
+                    © 2022 IFMT TADS, Inc
+                </p>
+            </footer>
+        </div>
+    )
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Footer from '../../components/Footer';
+
 function Home(){
     return(
         <div>
             Home do sistema!
-        </div>
+        </div>,
+
+        <Footer />
     )
 }
 
