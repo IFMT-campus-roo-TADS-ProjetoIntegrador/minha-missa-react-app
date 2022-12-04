9229d4a545659415d5abdc3edbc728f551e7f810
Jean <etudesaime1981@gmail.com>
2022-12-04 19:18:59 -0300

    Criação de Componente CallToAction
     Criação de componente CallToAction para a pagina Home

--- src/components/CallToAction/index.jsx
+++ src/components/CallToAction/index.jsx
@@ -10,11 +10,11 @@ export default function Text(){
     return(
         <Container>  
             <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold"> Bora para Missa? </h1>
+                <h1 class="display-5 fw-bold"> Bora para Missa ? </h1>
                 <p class="col-md-8 fs-4 text">
                 Os maiores  eventos da igreja  católica de Rondonópolis e da região, com informações privilegiadas estão aqui, confira!
                 </p>
-                <button class="btn btn-primary btn-lg" type="button" onClick={ () => history.push('/travels')}> Conferir aqui os eventos  da semana! </button>
+                <button class="btn btn-primary btn-lg" type="button" onClick={ () => history.push('/travels')}> Conferir aqui os eventos  da semana!</button>
             </div>
         </Container>
     )
