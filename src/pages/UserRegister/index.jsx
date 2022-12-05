c36ead563db988fe14c9a1d966af43709bb69ebf
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-12-05 13:10:33 -0300

    Criação da página de registro de usuário e criação da rota

---
+++ src/pages/UserRegister/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+// import Register from '../../components/RegisterForm/';
+
+export default function RegisterPage(){
+    return(
+        <div>
+            <Header />
+
+                {/* <Register /> */}
+                <h1>
+                    Registro de Usuário
+                </h1>
+                <span>Em desenvolvimento...</span>
+
+            <Footer />
+        </div>
+    )
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Home from '../pages/Home';
 import Auth from '../pages/Auth';
 import Events from '../pages/Events';
+import UserRegister from '../pages/UserRegister';
 
 function Router()
 {
@@ -14,6 +15,7 @@ function Router()
                 <Route path="/home" component={Home} />
                 <Route path="/login" component={Auth} />
                 <Route path="/events" component={Events} />
+                <Route path="/user-register" component={UserRegister} />
             </Switch>
         </BrowserRouter>
     )
