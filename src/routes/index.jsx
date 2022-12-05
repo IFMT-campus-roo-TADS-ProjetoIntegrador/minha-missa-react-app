1847cb735c230ccfd717b14452708d25af1541c4
Felipe Queiroz Bueno <64038516+Felipe-Queiroz-Bueno@users.noreply.github.com>
2022-12-05 11:50:07 -0300

    Inserindo a Página de eventos nas rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Auth from '../pages/Auth';
+import Events from '../pages/Events';
 
 function Router()
 {
@@ -12,6 +13,7 @@ function Router()
                 <Redirect exact to="/home" from="/" />
                 <Route path="/home" component={Home} />
                 <Route path="/login" component={Auth} />
+                <Route path="/events" component={Events} />
             </Switch>
         </BrowserRouter>
     )
